6f9730ad521215cb4573c238cb1231e523a365f9
Igor Gomes <igor.barbosa.tads@gmail.com>
2026-05-05 20:11:46 -0400

    Refactor saudacao function to accept time string

--- Atividade5.py
+++ Atividade5.py
@@ -1,11 +1,17 @@
-import time
+def saudacao(horario):
+    hora, minuto = horario.split(":")
+    hora = int(hora)
+    minuto = int (minuto)
+    if hora < 0 or hora > 23 or minuto < 0 or minuto > 59:
+        return "Horario Invalido"
 
-def saudacao(n):
-    if n > 5 and n < 12:
+    if hora >= 5 and hora <= 11:
         return "Bom Dia"
-    elif n >= 12 and n < 18:
+    elif hora >= 12 and hora <= 17:
         return "Boa Tarde"
     else:
         return "Boa Noite"
     
-print (saudacao(14.30))   
+print (saudacao("14:30"))
+print (saudacao("20:10"))
+print (saudacao("03:40"))
